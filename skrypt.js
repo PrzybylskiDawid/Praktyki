1f0cf06e831184409b2ece019a83b2e604505d62
PrzybylskiDawid <94458468+PrzybylskiDawid@users.noreply.github.com>
2022-11-07 23:42:17 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,25 @@
 function waliduj(){
+
     var imie = document.getElementById('imie').value;
-    var imieregex = /[A-ZÀ-ÿ][a-z]*/;
+    var imieregex = /^[A-Z][a-z]+$/;
     var imiewynik = imieregex.test(imie)
+
+    var nazwisko = document.getElementById('nazwisko').value;
+    var nazwiskoregex = /^[A-Z][a-z]+$/;
+    var nazwiskowynik = nazwiskoregex.test(nazwisko)
+
+    var urodziny = document.getElementById('urodziny').value;
+    var urodzinyregex = /^([01-31]+-[01-12]+-\d{4})$/i;
     
-    if(imiewynik = true)
+    var urodzinywynik = urodzinyregex.test(urodziny)
+    
+    if(imiewynik == true && nazwiskowynik == true && urodzinywynik == true)
     {
-        document.getElementById('wynik').innerHTML = 'poprawnie';
+        document.getElementById("walidacja").innerHTML = "brawo";
     }
     else
     {
-        document.getElementById('wynik').innerHTML = 'skill issue';
+        document.getElementById("walidacja").innerHTML = "źle";
     }
 }
+
