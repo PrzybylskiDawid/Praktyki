b4ae2fc6378d6d732ae2dc02cd2404db97d498e2
PrzybylskiDawid <94458468+PrzybylskiDawid@users.noreply.github.com>
2022-11-07 20:50:12 +0100

    Create skrypt.js

---
+++ skrypt.js
@@ -0,0 +1,10 @@
+function waliduj(){
+    var imie = document.getElementById('imie').value;
+    var imieregex = /[A-ZÀ-ÿ][a-z]*/;
+    var imiewynik = imieregex.test(imie)
+    
+    if(imiewynik = true)
+    {
+        document.getElementById('wynik').innerHTML = 'poprawnie';
+    }
+}
